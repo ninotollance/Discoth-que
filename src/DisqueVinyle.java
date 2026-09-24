01863befe6aa3e99e2d55e9f0827f000dae69df2
ErinLeDreau <erin.ledreau@gmail.com>
2026-09-24 11:38:41 +0200

    héritage Album.java

---
+++ src/DisqueVinyle.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+
+public class DisqueVinyle extends Album{
+
+    private String numeroSerie;
+    private String type;
+
+    public DisqueVinyle(String nom, Auteur auteur, LocalDate annee, int quantite, String numeroSerie, String type) {
+        super(nom, auteur, annee, quantite);
+        this.numeroSerie = numeroSerie;
+        this.type = type;
+    }
+
+    public String getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(String numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
